a84a101d73e9b84a88b7804e5e80f5857cfbbaf5
Arlaine Sanchez <amaesanchez@gmail.com>
2023-01-10 19:51:42 -0500

    finish testing shipments

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -3,7 +3,6 @@
 const request = require("supertest");
 const app = require("../app");
 
-
 describe("POST /", function () {
   test("valid", async function () {
     const resp = await request(app).post("/shipments").send({
@@ -17,9 +16,27 @@ describe("POST /", function () {
   });
 
   test("throws error if empty request body", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send();
+    const resp = await request(app).post("/shipments").send();
     expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error).toEqual({
+      message: ["instance is required, but is undefined"],
+      status: 400,
+    });
+  });
+
+  test("throws error if invalid request body", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "dynamite",
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error).toEqual({
+      message: [
+        "instance requires property \"addr\"",
+        "instance requires property \"zip\""
+      ],
+      status: 400,
+    });
   });
 });
