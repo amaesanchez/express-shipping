87a73de8e47d13f1463626678de475ced78f93f8
Chalon Lubin <chalonlubin1@gmail.com>
2023-01-10 17:16:55 -0800

    swapping for shipIt mocks

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,16 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
+const request = require("supertest");
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
 
+const { shipProduct, SHIPIT_SHIP_URL } = require("./shipItApi");
 
 test("shipProduct", async function () {
+
+  axiosMock.onGet(`${SHIPIT_SHIP_URL}`).reply(200, 5005);
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +18,6 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(5005);
+  expect(res.statusCode).toEqual(200);
 });
